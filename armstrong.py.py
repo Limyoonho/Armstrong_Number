ce2cb75a4dd74cc65a81175f23b375edcad5195f
shreyadas795 <57699346+shreyadas795@users.noreply.github.com>
2021-03-16 22:42:35 +0530

    Update armstrong.py.py

--- armstrong.py.py
+++ armstrong.py.py
@@ -1,12 +1,17 @@
-a=int(input("Enter the number that you want to check if its armstrong number or not::"))
-t=a
+'''armstrong number : 153=1^3+5^3+3^3
+                    1+125+27=153
+                    its a armstrong number'''
+
+#code
+a=int(input("Enter the number that you want to check if its armstrong number or not::")) #input for the number
+t=a 
 s=0
-n=len(str(a))
+n=len(str(a)) #for the length of the number
 while t>0:
-    k=t%10
-    s+=k**n
+    k=t%10 #for separation of the number 
+    s+=k**n # "**" is used for power 
     t//=10
-if s==a:
+if s==a: #if the sum of the power value is equal to the given number 
     print("ARMSTRONG NUMBER!!!!")
 else:
     print("Not a armstrong number !!!")
