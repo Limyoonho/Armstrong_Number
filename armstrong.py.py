0e5a657e7a0f2d0bf527e811acd004f0cd5ebc48
Limyoonho <ylim8689@naver.com>
2022-04-29 12:39:02 +0000

    Add Description

--- armstrong.py.py
+++ armstrong.py.py
@@ -2,17 +2,17 @@
                     1+125+27=153
                     its a armstrong number'''
 
-#code
-num = int(input("Enter the number that you want to check if its armstrong number or not::")) #input for the number
+# code
+num = int(input("Enter the number that you want to check if it's armstrong number or not : ")) # 판단할 숫자 입력
 sum = 0
-temp = num
+temp = num                  # num의 값을 while에서 바꾸기 위해 설정
 
-while temp > 0:
-    digit = temp % 10 #for separation of the number 
-    sum += digit ** 3 # "**" is used for power 
-    temp //= 10
+while temp > 0:             # 입력 수의 몫이 0이 될 때까지 반복
+    digit = temp % 10       # 입력숫자를 10으로 나눈 나머지 값으로 각 자리 숫자를 찾음
+    sum += digit ** 3       # 암스트롱의 수를 구하기 위해 자릿수를 세제곱 후 합산
+    temp //= 10             # temp에 몫 값을 설정함
 
-if sum == num: #if the sum of the power value is equal to the given number 
-    print("ARMSTRONG NUMBER!!!!")
+if sum == num:              # 각 자릿수의 세제곱한 값의 총합과 입력값이 일치하는 지 비교
+    print(num, "IS ARMSTRONG NUMBER!!!")
 else:
-    print("Not a armstrong number !!!")
+    print(num, "is not a armstrong number!!!")
