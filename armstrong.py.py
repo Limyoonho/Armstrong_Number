39df50c86fe172c9d9c9d3161c40d56924d5467e
Limyoonho <ylim8689@naver.com>
2022-04-29 12:28:34 +0000

    GitPod Modified

--- armstrong.py.py
+++ armstrong.py.py
@@ -3,15 +3,16 @@
                     its a armstrong number'''
 
 #code
-a=int(input("Enter the number that you want to check if its armstrong number or not::")) #input for the number
-t=a 
-s=0
-n=len(str(a)) #for the length of the number
-while t>0:
-    k=t%10 #for separation of the number 
-    s+=k**n # "**" is used for power 
-    t//=10
-if s==a: #if the sum of the power value is equal to the given number 
+num = int(input("Enter the number that you want to check if its armstrong number or not::")) #input for the number
+sum = 0
+temp = num
+
+while temp > 0:
+    digit = temp % 10 #for separation of the number 
+    sum += digit ** 3 # "**" is used for power 
+    temp //= 10
+
+if sum == num: #if the sum of the power value is equal to the given number 
     print("ARMSTRONG NUMBER!!!!")
 else:
     print("Not a armstrong number !!!")
